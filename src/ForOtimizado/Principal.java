b6faf4a2c6f299fcebaa053421bde24be61b4cc4
Vitor Hideo Murakami <vitorhideomurakami@gmail.com>
2021-11-18 22:25:27 -0300

    Comentário sobre o for otimizado corrigido

--- src/ForOtimizado/Principal.java
+++ src/ForOtimizado/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 		String array[] = {"João", "Paulo", "Maria", "Ricardo", "Gabriela"};
 		
-		//o for otimizado irá percorrer todo o array, e o valor de cada posição será atribuida ao atribudo declarado
+		//o for otimizado irá percorrer todo o array, e o valor de cada posição será atribuida a variável declarada
 		for (String variavel: array) {
 			System.out.println(variavel);
 		}
